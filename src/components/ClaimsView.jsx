4525caba86972185ecd37bf82863d8260002e388
Utavegu <utavegu@list.ru>
2021-07-15 13:24:02 +0500

    Закончил мультифильтр и ещё немного прибрался в компоненте Filters

--- src/components/ClaimsView.jsx
+++ src/components/ClaimsView.jsx
@@ -6,9 +6,8 @@ import Search from './Search';
 
 /*
 ISSUES:
-  1) Сделать сброс фильтров при добавлении / удалении / редактировании
-  2) Возможно хэндлГетФильтрс стоит переделать
-  3) Да тут всё теперь стоит переделать >_<
+  - Оптимизировать поиск
+  - Подумать над лишними пробросами через переменные в хэндлГетФильтрз
 */
 
 const ClaimsView = ({claims, onEdit: handleSelectClaim, onDelete: handleDeleteClaim}) => {
@@ -24,57 +23,20 @@ const ClaimsView = ({claims, onEdit: handleSelectClaim, onDelete: handleDeleteCl
   )
 
   const handleGetFilters = (filtersState) => {
-    console.log(filtersState);
-    // Что-то пошло не так... Может оллДэйт и пр. тоже наверх продёрнуть и вместо клэймс подставлять? Или вообще не продёргивать, а прямо сюда эту функцию вынести и наоборот опускать вниз...
+    // С одной стороны - лишние пробросы через переменные, с другой - так читабельнее... Подумаю ещё над этим
+    const dateFilter = claim => (filtersState.date === "all" || claim.datetime.slice(0, -7) === filtersState.date);
+    const firmFilter = claim => (filtersState.firm === "all" || claim.firmName === filtersState.firm);
+    const carrierFilter = claim => (filtersState.carrier === "all" || claim.fullname === filtersState.carrier);
 
-    filtersState.date === "all" ? setFiltredClaims(claims) : setFiltredClaims(
-      claims.slice().filter(claim => claim.datetime.slice(0, -7) === filtersState.date)
-    )
-    &&
-    filtersState.firm === "all" ? setFiltredClaims(claims) : setFiltredClaims(
-      claims.slice().filter(claim => claim.firmName === filtersState.firm)
-    )
-    &&
-    filtersState.carrier === "all" ? setFiltredClaims(claims) : setFiltredClaims(
-      claims.slice().filter(claim => claim.fullname === filtersState.carrier)
-    ) // Так, в данный момент ты правильно реагируешь именно на перевозчика. Но только если в других пунктах нет "всех заявок"... Так, думай... Нет, только если в первом пункте "все".
-    // Давай ещё раз. В датах не должны быть выбраны "все даты", в фирмах может быть что угодно. Тогда работает фильтр по перевозчику. Но только если там не все перевозчики. Я уверен, ещё одна кружка чая и я приду к разгадке.
+    const multifilter = claims
+      .filter(dateFilter)
+      .filter(firmFilter)
+      .filter(carrierFilter);
+
+    setFiltredClaims(multifilter);
   };
 
-  /*
-  Блин, вспоминай как это делать... =/
 
-  // Фильтр по ТИПУ ЖИЛЬЯ
-  function filterHousingType(element) {
-    return housingType.value === 'any' ? true : element.offer.type === housingType.value;
-  }
-
-  // Фильтр по КОЛИЧЕСТВУ КОМНАТ
-  function filterHousingRooms(element) {
-    return housingRooms.value === 'any' ? true : element.offer.rooms === Number(housingRooms.value);
-  }
-
-  // Фильтр по ЧИСЛУ ГОСТЕЙ
-  function filterHousingGuests(element) {
-    return housingGuests.value === 'any' ? true : element.offer.guests === Number(housingGuests.value);
-  }
-
-  // ФУНКЦИЯ, объединяющая работу всех фильров
-  function multyfilter(data) {
-    return data
-      .filter(function (element) {
-        return (
-          filterHousingType(element) &&
-          filterHousingRooms(element) &&
-          filterHousingGuests(element)
-        );
-      })
-  }
-  */
-
-
-
-  // Поиск допилить по ATI и телефону ещё. По номеру заявки? По радиобаттону... Или можно через двойное(тройное) "или" искать по другим двум поляем ещё. А потом отдавать уникальный массив, без повторений. Хотя с такими полями какие уж там повторения...
   const foundClaims = filtredClaims
     .slice()
     .filter(claim => 
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -6,6 +6,26 @@ const Filters = ({claims, onGetFilters: handleGetFilters}) => {
 
   const INITIAL_VALUE = "all";
 
+  const getUniqueData = () => {
+    const allDates = new Set();
+    const allFirms = new Set();
+    const allCarrier = new Set();
+
+    for (const claim of claims) {
+      allDates.add(claim.datetime.slice(0, -7));
+      allFirms.add(claim.firmName);
+      allCarrier.add(claim.fullname);
+    }
+
+    return {
+      dates: Array.from(allDates),
+      firms: Array.from(allFirms),
+      carriers: Array.from(allCarrier),
+    };
+  }
+
+  const {dates, firms, carriers} = getUniqueData();
+
   const [filters, setFilters] = useState({
     date: INITIAL_VALUE,
     firm: INITIAL_VALUE,
@@ -25,45 +45,27 @@ const Filters = ({claims, onGetFilters: handleGetFilters}) => {
 
   useEffect(
     () => {
-      setFilters(prevForm => ({...prevForm, date: INITIAL_VALUE, firm: INITIAL_VALUE, carrier: INITIAL_VALUE}));
+      setFilters({date: INITIAL_VALUE, firm: INITIAL_VALUE, carrier: INITIAL_VALUE});
     },
     [claims]
   );
 
-  const getUniqueData = () => {
-    const allDates = new Set();
-    const allFirms = new Set();
-    const allCarrier = new Set();
-
-    for (const claim of claims) {
-      allDates.add(claim.datetime.slice(0, -7));
-      allFirms.add(claim.firmName);
-      allCarrier.add(claim.fullname);
-    }
-
-    return {
-      dates: Array.from(allDates),
-      firms: Array.from(allFirms),
-      carriers: Array.from(allCarrier),
-    };
-  }
-
-
+  
   return (
     <form>
       <select name="date" value={filters.date} onChange={handleInputChange}>
         <option value="all">Все даты</option>
-        {getUniqueData().dates.map(date => <option key={date} value={date}>{date}</option>)}
+        {dates.map(date => <option key={date} value={date}>{date}</option>)}
       </select>
 
       <select name="firm" value={filters.firm} onChange={handleInputChange}>
         <option value="all">Все фирмы</option>
-        {getUniqueData().firms.map(firm => <option key={firm} value={firm}>{firm}</option>)}
+        {firms.map(firm => <option key={firm} value={firm}>{firm}</option>)}
       </select>
 
       <select name="carrier" value={filters.carrier} onChange={handleInputChange}>
         <option value="all">Все перевозчики</option>
-        {getUniqueData().carriers.map(carrier => <option key={carrier} value={carrier}>{carrier}</option>)}
+        {carriers.map(carrier => <option key={carrier} value={carrier}>{carrier}</option>)}
       </select>
     </form>
   )
